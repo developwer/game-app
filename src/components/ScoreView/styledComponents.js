d095a500689bf3a9e4ddd0d334d8e9768a8cf6ce
rahul <rahul@gmail.com>
2024-04-06 06:53:34 +0000

    solution

---
+++ src/components/ScoreView/styledComponents.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const ScoreContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  width: 80%;
+  border-style: solid;
+  border-color: white;
+  border-width: 2px;
+  border-radius: 10px;
+  background-color: transparent;
+  margin-top: 2%;
+`
+
+export const ScoreNameContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const ScoreName = styled.h1`
+  color: white;
+  font-size: 20px;
+  font-family: Bree Serif;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`
+
+export const ScoreBoard = styled(ScoreNameContainer)`
+  background-color: white;
+  width: 15%;
+  height: 100%;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+`
+export const ScoreHeading = styled.p`
+  color: #223a5f;
+  font-size: 20px;
+  margin-bottom: 0px;
+`
+
+export const ScoreResults = styled.p`
+  color: #223a5f;
+  font-size: 42px;
+  margin-bottom: 0px;
+  font-family: 'Roboto';
+`
